c879d9234f06357afd7fb3a965f8432bb82bf528
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-05-24 10:42:36 +0200

    Hello/World.sh: print arguments if there are any

--- Hello/World.sh
+++ Hello/World.sh
@@ -2,4 +2,7 @@
 
 echo
 echo "Hello, World! When you see this message, the Linux Command Line Tools are working as expected!"
+if ! [[ "$@" == "" ]]; then
+    echo "This is script is ran with the following arguments: '$@'."
+fi
 echo
