659bffb0a36113be6e27fc0795089de5420dc827
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-05-24 12:49:15 +0200

    Hello/World.sh: check dependencies

--- Hello/World.sh
+++ Hello/World.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo"
+"$(which bash)" "$VAR_BIN_INSTALL_DIR/bin/CheckDependencies.sh" "$VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS" || { exit 1; }
 
 echo
 echo "Hello, World! When you see this message, the Linux Command Line Tools are working as expected!"
