7effd6b6ef48609ba20d484083b6b43e86b4f163
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-05-24 11:25:41 +0200

    Plesk/CalculateMailStatistics.sh: print mail count per email address

--- Plesk/CalculateMailStatistics.sh
+++ Plesk/CalculateMailStatistics.sh
@@ -17,7 +17,7 @@ for var_domain in "$VAR_SYSTEM_MAIL_DIR"/*; do
     echo "Processing Domain: $(basename $var_domain)..."
     for var_email_address in "$var_domain"/*; do
         if [ -d $var_email_address ]; then
-            echo " - $(basename "$var_email_address")@$(basename "$var_domain")"
+            echo " - $(basename "$var_email_address")@$(basename "$var_domain"): $(tree -a $var_email_address | grep -c $VAR_SYSTEM_HOSTNAME)"
         fi
     done
     echo "Done!"
