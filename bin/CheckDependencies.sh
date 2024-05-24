a54a671bafda03711c78b503931abd7973a62db2
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-05-24 12:48:33 +0200

    Create bin/CheckDependencies.sh

---
+++ bin/CheckDependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echoDebug "Dependency check will happen here..."
+echoDebug "The following dependencies will be checked: '$@'..."
+
+for var_script_dependency in $@; do
+    if [ $(which "$var_script_dependency") ]; then
+        echo "Dependency '$var_script_dependency' found!"
+    else
+        echo "Dependency '$var_script_dependency' was not found on this system. Exiting..."
+        exit 1
+    fi
+done
