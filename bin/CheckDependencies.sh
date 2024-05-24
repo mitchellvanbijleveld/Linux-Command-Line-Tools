ce57a1edc63ee63dae90fc301c684507072fb6bc
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-05-24 12:53:05 +0200

    bin/CheckDependencies.sh: only print missing dependencies unless debugging

--- bin/CheckDependencies.sh
+++ bin/CheckDependencies.sh
@@ -5,7 +5,7 @@ echoDebug "The following dependencies will be checked: '$@'..."
 
 for var_script_dependency in $@; do
     if [ $(which "$var_script_dependency") ]; then
-        echo "Dependency '$var_script_dependency' found!"
+        echoDebug "Dependency '$var_script_dependency' found!"
     else
         echo "Dependency '$var_script_dependency' was not found on this system. Exiting..."
         exit 1
