d43c79e3289fb84308ebc0af911ff91f5d0b6f7a
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-05-24 17:44:30 +0200

    Plesk/CalculateMailStatistics.sh: print statistics from dictionary

--- Plesk/CalculateMailStatistics.sh
+++ Plesk/CalculateMailStatistics.sh
@@ -7,25 +7,36 @@ VAR_SYSTEM_HOSTNAME=$(hostname)
 
 VAR_STATISTICS_MAIL_COUNT_TOTAL=$(tree -a $VAR_SYSTEM_MAIL_DIR | grep -c $VAR_SYSTEM_HOSTNAME)
 
+declare -A VAR_STATISTICS
+
 PrintStatistics(){
     echo "===== MAIL STATISTICS ====="
     echo " - The Total Mail Count is: $VAR_STATISTICS_MAIL_COUNT_TOTAL"
     echo ""
+    for var_email_address_string in "${!VAR_STATISTICS[@]}"; do
+        echo " - $var_email_address_string: ${VAR_STATISTICS[$var_email_address_string]}"
+    done
 }
 
-PrintStatistics
-
 for var_domain in "$VAR_SYSTEM_MAIL_DIR"/*; do
-    echo "Processing Domain: $(basename $var_domain)..."
+    echoDebug "Processing Domain: $(basename $var_domain)..."
     for var_email_address in "$var_domain"/*; do
         if [ -d $var_email_address ]; then
+            var_email_address_string=$(basename "$var_email_address")@$(basename "$var_domain")
+
             var_statistics_mail_count_total=$(tree -a $var_email_address | grep -c $VAR_SYSTEM_HOSTNAME)
             var_statistics_mail_count_inbox=$(tree -a -I '.Drafts|.Sent|.Spam' "$var_email_address/Maildir/" | grep -c $VAR_SYSTEM_HOSTNAME)
             var_statistics_mail_count_spam=$(tree -a "$var_email_address/Maildir/.Spam/" | grep -c $VAR_SYSTEM_HOSTNAME)
             var_statistics_mail_count_sent=$(tree -a "$var_email_address/Maildir/.Sent/" | grep -c $VAR_SYSTEM_HOSTNAME)
-            echo " - $(basename "$var_email_address")@$(basename "$var_domain"): $var_statistics_mail_count_total (Inbox: $var_statistics_mail_count_inbox, Spam: $var_statistics_mail_count_spam, Sent: $var_statistics_mail_count_sent)"
+
+            var_statistics_string="$var_statistics_mail_count_total (Inbox: $var_statistics_mail_count_inbox, Spam: $var_statistics_mail_count_spam, Sent: $var_statistics_mail_count_sent)"
+
+            echoDebug " - $var_email_address_string: $var_statistics_mail_count_total"
+            VAR_STATISTICS[$var_email_address_string]="$var_statistics_string"
         fi
     done
-    echo "Done!"
-    echo
+    echoDebug "Done!"
+    echoDebug
 done
+
+PrintStatistics
