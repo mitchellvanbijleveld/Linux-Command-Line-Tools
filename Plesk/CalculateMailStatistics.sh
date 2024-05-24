5227a7263c2332077384ab8ac937e50f55dbcb1c
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-05-24 18:05:16 +0200

    Plesk/CalculateMailStatistics.sh: reformat statistics

--- Plesk/CalculateMailStatistics.sh
+++ Plesk/CalculateMailStatistics.sh
@@ -6,6 +6,7 @@ VAR_SYSTEM_MAIL_DIR="/var/qmail/mailnames"
 VAR_SYSTEM_HOSTNAME=$(hostname)
 
 VAR_STATISTICS_MAIL_COUNT_TOTAL=$(tree -a $VAR_SYSTEM_MAIL_DIR | grep -c $VAR_SYSTEM_HOSTNAME)
+VAR_STATISTICS_PROCESSED_EMAIL_ACCOUNTS=0
 
 declare -A VAR_STATISTICS
 VAR_STATISTICS["INBOX"]=0
@@ -15,11 +16,12 @@ declare -A VAR_STATISTICS_EMAIL_ADDRESS
 
 PrintStatistics(){
     echo "===== MAIL STATISTICS ====="
-    echo " - The Total Mail Count is: $VAR_STATISTICS_MAIL_COUNT_TOTAL"
+    echo "The Total Mail Count is: $VAR_STATISTICS_MAIL_COUNT_TOTAL"
     for var_mail_box_count in "${!VAR_STATISTICS[@]}"; do
         echo " - $var_mail_box_count: ${VAR_STATISTICS[$var_mail_box_count]}"
     done
     echo ""
+    echo "The total amount of email addresses: $VAR_STATISTICS_PROCESSED_EMAIL_ACCOUNTS"
     for var_email_address_string in "${!VAR_STATISTICS_EMAIL_ADDRESS[@]}"; do
         echo " - $var_email_address_string: ${VAR_STATISTICS_EMAIL_ADDRESS[$var_email_address_string]}"
     done
@@ -32,6 +34,7 @@ for var_domain in "$VAR_SYSTEM_MAIL_DIR"/*; do
     for var_email_address in "$var_domain"/*; do
         if [ -d $var_email_address ]; then
             var_email_address_string=$(basename "$var_email_address")@$(basename "$var_domain")
+            ((VAR_STATISTICS_PROCESSED_EMAIL_ACCOUNTS+=1))
 
             var_statistics_mail_count_total=$(tree -a $var_email_address | grep -c $VAR_SYSTEM_HOSTNAME)
             var_statistics_mail_count_inbox=$(tree -a -I '.Drafts|.Sent|.Spam' "$var_email_address/Maildir/" | grep -c $VAR_SYSTEM_HOSTNAME)
