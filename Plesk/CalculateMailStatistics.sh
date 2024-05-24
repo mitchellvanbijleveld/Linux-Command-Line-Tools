61c01b23b7cb12ec2d69e9e7c711f189375b549c
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-05-24 18:44:38 +0200

    Plesk/CalculateMailStatistics.sh: print mail count per day

--- Plesk/CalculateMailStatistics.sh
+++ Plesk/CalculateMailStatistics.sh
@@ -14,6 +14,10 @@ VAR_STATISTICS["SPAM"]=0
 VAR_STATISTICS["SENT"]=0
 declare -A VAR_STATISTICS_EMAIL_ADDRESS
 
+declare -A VAR_STATISTICS_MAIL_PER_DATE_INBOX
+declare -A VAR_STATISTICS_MAIL_PER_DATE_SPAM
+declare -A VAR_STATISTICS_MAIL_PER_DATE_SENT
+
 PrintStatistics(){
     echo "===== MAIL STATISTICS ====="
     echo "The Total Mail Count is: $VAR_STATISTICS_MAIL_COUNT_TOTAL"
@@ -26,6 +30,20 @@ PrintStatistics(){
         echo " - $var_email_address_string: ${VAR_STATISTICS_EMAIL_ADDRESS[$var_email_address_string]}"
     done
     echo ""
+    echo "Mail Per Date:"
+    echo " - Inbox"
+    for date in $(for date in "${!VAR_STATISTICS_MAIL_PER_DATE_INBOX[@]}"; do echo "$date"; done | sort); do
+        echo "   $date: ${VAR_STATISTICS_MAIL_PER_DATE_INBOX[$date]}"
+    done
+    echo " - Spam"
+    for date in $(for date in "${!VAR_STATISTICS_MAIL_PER_DATE_SPAM[@]}"; do echo "$date"; done | sort); do
+        echo "   $date: ${VAR_STATISTICS_MAIL_PER_DATE_SPAM[$date]}"
+    done
+    echo " - Sent"
+    for date in $(for date in "${!VAR_STATISTICS_MAIL_PER_DATE_SENT[@]}"; do echo "$date"; done | sort); do
+        echo "   $date: ${VAR_STATISTICS_MAIL_PER_DATE_SENT[$date]}"
+    done
+    echo
 
 }
 
@@ -39,10 +57,23 @@ for var_domain in "$VAR_SYSTEM_MAIL_DIR"/*; do
             var_statistics_mail_count_total=$(tree -a $var_email_address | grep -c $VAR_SYSTEM_HOSTNAME)
             var_statistics_mail_count_inbox=$(tree -a -I '.Drafts|.Sent|.Spam' "$var_email_address/Maildir/" | grep -c $VAR_SYSTEM_HOSTNAME)
             ((VAR_STATISTICS["INBOX"]+=var_statistics_mail_count_inbox))
+            while IFS= read -r date; do
+                ((VAR_STATISTICS_MAIL_PER_DATE_INBOX["$date"]++))
+            done < <(find "$var_email_address/Maildir/" -type f -iname "*$VAR_SYSTEM_HOSTNAME*" -printf '%TY-%Tm-%Td\n')
+
             var_statistics_mail_count_spam=$(tree -a "$var_email_address/Maildir/.Spam/" | grep -c $VAR_SYSTEM_HOSTNAME)
             ((VAR_STATISTICS["SPAM"]+=var_statistics_mail_count_spam))
+            while IFS= read -r date; do
+                    ((VAR_STATISTICS_MAIL_PER_DATE_SPAM["$date"]++))
+                    ((VAR_STATISTICS_MAIL_PER_DATE_INBOX["$date"]--))
+            done < <(find "$var_email_address/Maildir/.Spam/" -type f -iname "*$VAR_SYSTEM_HOSTNAME*" -printf '%TY-%Tm-%Td\n')
+
             var_statistics_mail_count_sent=$(tree -a "$var_email_address/Maildir/.Sent/" | grep -c $VAR_SYSTEM_HOSTNAME)
             ((VAR_STATISTICS["SENT"]+=var_statistics_mail_count_sent))
+            while IFS= read -r date; do
+                    ((VAR_STATISTICS_MAIL_PER_DATE_SENT["$date"]++))
+                    ((VAR_STATISTICS_MAIL_PER_DATE_INBOX["$date"]--))
+            done < <(find "$var_email_address/Maildir/.Sent/" -type f -iname "*$VAR_SYSTEM_HOSTNAME*" -printf '%TY-%Tm-%Td\n')
 
             var_statistics_string="$var_statistics_mail_count_total (Inbox: $var_statistics_mail_count_inbox, Spam: $var_statistics_mail_count_spam, Sent: $var_statistics_mail_count_sent)"
 
