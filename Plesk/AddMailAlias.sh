b25f55f51cae5483df57e3dd8fb92a5a97dd8243
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-05-29 19:19:40 +0200

    Plesk/AddMailAlias.sh: replace echo with echoInfo

--- Plesk/AddMailAlias.sh
+++ Plesk/AddMailAlias.sh
@@ -6,17 +6,17 @@ VAR_EMAIL_ADDRESS=$1
 VAR_EMAIL_ADDRESS_ALIAS=$2
 
 if [[ $VAR_EMAIL_ADDRESS == "" ]]; then
-    echo "Please provide an email address!"
+    echoInfo "Please provide an email address!"
     exit 1
 fi
 
 if [[ $VAR_EMAIL_ADDRESS_ALIAS == "" ]]; then
-    echo "Please provide an alias for this email address!"
+    echoInfo "Please provide an alias for this email address!"
     exit 1
 fi
 
 VAR_EMAIL_ADDRESS_NEW_WITH_ALIAS="$VAR_EMAIL_ADDRESS_ALIAS@$(echo "$VAR_EMAIL_ADDRESS" | awk -F '@' '{print $2}')"
 
-echo "Adding alias '$VAR_EMAIL_ADDRESS_ALIAS' to email address '$VAR_EMAIL_ADDRESS'..."
-echo "New email address / alias: '$VAR_EMAIL_ADDRESS_NEW_WITH_ALIAS'."
+echoInfo "Adding alias '$VAR_EMAIL_ADDRESS_ALIAS' to email address '$VAR_EMAIL_ADDRESS'..."
+echoInfo "New email address / alias: '$VAR_EMAIL_ADDRESS_NEW_WITH_ALIAS'."
 plesk bin mail -u "$VAR_EMAIL_ADDRESS" -aliases "add:$VAR_EMAIL_ADDRESS_ALIAS"
