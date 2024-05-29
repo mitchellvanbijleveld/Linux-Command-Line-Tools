8a5ab61a1a383e0e750bcde17e36998c1884c2c8
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-05-29 19:17:20 +0200

    Hello/World.sh: replace echo with echoInfo

--- Hello/World.sh
+++ Hello/World.sh
@@ -2,9 +2,9 @@
 VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo"
 "$(which bash)" "$VAR_BIN_INSTALL_DIR/bin/CheckDependencies.sh" "$VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS" || { exit 1; }
 
-echo
-echo "Hello, World! When you see this message, the Linux Command Line Tools are working as expected!"
+echoInfo
+echoInfo "Hello, World! When you see this message, the Linux Command Line Tools are working as expected!"
 if ! [[ "$@" == "" ]]; then
-    echo "This is script is ran with the following arguments: '$@'."
+    echoInfo "This is script is ran with the following arguments: '$@'."
 fi
-echo
+echoInfo
