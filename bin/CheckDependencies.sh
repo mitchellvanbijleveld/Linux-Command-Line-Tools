1d4ac28e837f8d5c2abf50ea365d9b6e258a43fd
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-05-29 19:20:23 +0200

    bin/CheckDependencies.sh: replace echo with echoInfo

--- bin/CheckDependencies.sh
+++ bin/CheckDependencies.sh
@@ -7,7 +7,7 @@ for var_script_dependency in $@; do
     if [ $(which "$var_script_dependency") ]; then
         echoDebug "Dependency '$var_script_dependency' found!"
     else
-        echo "Dependency '$var_script_dependency' was not found on this system. Exiting..."
+        echoInfo "Dependency '$var_script_dependency' was not found on this system. Exiting..."
         exit 1
     fi
 done
