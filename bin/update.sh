6536f216bd7c3a13ee757831c7040a43fe80d218
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-06-01 21:46:55 +0200

    bin/update.sh: fix printing empty lines

--- bin/update.sh
+++ bin/update.sh
@@ -6,7 +6,7 @@ VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo git"
 "$(which bash)" "$VAR_BIN_INSTALL_DIR/bin/CheckDependencies.sh" "$VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS" || { exit 1; }
 
 echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Updating..."
-echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" ""
+echoInfo
 git -C "$VAR_BIN_INSTALL_DIR" pull
-echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" ""
+echoInfo
 echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Done"
