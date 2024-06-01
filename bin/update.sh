c1a3f14b57ed2aec56f0ce7ea5351cdbd062395a
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-06-01 12:07:44 +0200

    bin/update.sh: add script info for echoInfo

--- bin/update.sh
+++ bin/update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+VAR_UTILITY="bin"
+VAR_UTILITY_SCRIPT="update"
+
 VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo git"
 "$(which bash)" "$VAR_BIN_INSTALL_DIR/bin/CheckDependencies.sh" "$VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS" || { exit 1; }
 
-echoInfo "Updating..."
-echoInfo ""
+echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Updating..."
+echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" ""
 git -C "$VAR_BIN_INSTALL_DIR" pull
-echoInfo ""
-echoInfo "Done"
+echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" ""
+echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Done"
