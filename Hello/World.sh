93948caafa5d8f27c2df9ed6cbe89e650be7aeb3
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-06-11 22:09:01 +0200

    Hello/World.sh: update echo to echoInfo and add flag helper

--- Hello/World.sh
+++ Hello/World.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+VAR_UTILITY="Hello"
+VAR_UTILITY_SCRIPT="World"
+
 VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo"
 "$(which bash)" "$VAR_BIN_INSTALL_DIR/bin/CheckDependencies.sh" "$VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS" || { exit 1; }
 
 echoInfo
-echoInfo "Hello, World! When you see this message, the Linux Command Line Tools are working as expected!"
+echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Hello, World! If you see this message, the Linux Command Line Tools are working as expected!"
 if ! [[ "$@" == "" ]]; then
-    echoInfo "This is script is ran with the following arguments: '$@'."
+    echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "This is script is ran with the following arguments: '$@'."
+else
+    echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Try adding some flags (--FLAG) to see if they are recognized!"
 fi
 echoInfo
