145620a99e58d85fc71dc6e5467b1ae27f792f7f
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-06-11 22:45:57 +0200

    Hello/World.sh: fix language

--- Hello/World.sh
+++ Hello/World.sh
@@ -8,7 +8,7 @@ VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo"
 echoInfo
 echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Hello, World! If you see this message, the Linux Command Line Tools are working as expected!"
 if ! [[ "$@" == "" ]]; then
-    echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "This is script is ran with the following arguments: '$@'."
+    echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "The script is run with the following arguments: '$@'."
 else
     echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Try adding some flags (--FLAG) to see if they are recognized!"
 fi
