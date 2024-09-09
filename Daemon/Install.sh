cefa835daaed4242f71bde750613b3354f7c303e
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-09-09 21:14:20 +0200

    Create Daemon/Install.sh

---
+++ Daemon/Install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+VAR_UTILITY="Daemon"
+VAR_UTILITY_SCRIPT="Install"
+VAR_UTILITY_SCRIPT_VERSION="2024.09.09-2048"
+VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo sed tmux"
+
+# Utility Script Variables
+VAR_DAEMON_NAME="$1"
+VAR_DAEMON_CONFIG_FILE=$(echo "$VAR_UTILITY_SCRIPT_CONFIG_DIR" | sed "s/$VAR_UTILITY_SCRIPT/$VAR_DAEMON_NAME/")
+VAR_DAEMON_EXAMPLE_FILE="$VAR_UTILITY_FOLDER_PATH/Examples/$VAR_DAEMON_NAME"
+
+if [[ $VAR_DAEMON_NAME == "" ]]; then
+    echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "No Daemon specified. Exiting..."
+    exit 1
+fi
+
+echo $VAR_DAEMON_CONFIG_FILE
+echo $VAR_DAEMON_EXAMPLE_FILE
+
+if [[ -f "$VAR_DAEMON_CONFIG_FILE" ]]; then
+    echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Daemon '$VAR_DAEMON_NAME' already installed. Exiting..."
+    exit 1
+fi
+
+if [[ ! -f "$VAR_DAEMON_EXAMPLE_FILE" ]]; then
+    echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Daemon '$VAR_DAEMON_NAME' does not exist as an example. Exiting..."
+    exit 1
+fi
+
+cp -v $VAR_DAEMON_EXAMPLE_FILE $VAR_DAEMON_CONFIG_FILE
+
+exit 0
