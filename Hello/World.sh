3f5f8c94d517efb776d8d3665624417b145da1ca
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-09-11 23:58:01 +0200

    Hello/World.sh: if there is more than one argument, print all

--- Hello/World.sh
+++ Hello/World.sh
@@ -6,7 +6,8 @@ VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo"
 echoInfo
 echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Hello, World! If you see this message, the Linux Command Line Tools are working as expected!"
 if ! [[ "$@" == "" ]]; then
-    echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "The script is run with the following arguments: '$@'."
+    ScriptArguments="$@"
+    echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "The script is run with the following arguments: '$ScriptArguments'."
 else
     echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Try adding some flags (--FLAG) to see if they are recognized!"
 fi
