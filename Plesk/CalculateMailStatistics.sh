5ca1429493e40c2ee75372cc8268a5950c3eed6c
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-09-12 22:47:32 +0200

    Plesk/CalculateMailStatistics.sh: always print date result

--- Plesk/CalculateMailStatistics.sh
+++ Plesk/CalculateMailStatistics.sh
@@ -428,19 +428,19 @@ PrintStatistics_Comparison_PerDate(){
         fi
     done
 
-    if [[ $VAR_FAIL_DATE -eq 1 ]] && [[ $VAR_SCRIPT_VERBOSE -eq 0 ]]; then
+    if [[ $VAR_FAIL_DATE -eq 1 ]]; then
         PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  NOT OK"
-    elif [[ $VAR_FAIL_DATE -eq 0 ]] && [[ $VAR_SCRIPT_VERBOSE -eq 0 ]]; then
+    elif [[ $VAR_FAIL_DATE -eq 0 ]]; then
         PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  OK"
     fi
 
-    if [[ $VAR_DATE_REPAIR -eq 0 ]] && [[ $VAR_DATE_REPAIR_FAIL -eq 0 ]] && [[ $VAR_SCRIPT_VERBOSE -eq 0 ]]; then
-        PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  NOTHING TO REPAIR & OK"
-    elif [[ $VAR_DATE_REPAIR -eq 1 ]] && [[ $VAR_DATE_REPAIR_FAIL -eq 0 ]] && [[ $VAR_SCRIPT_VERBOSE -eq 0 ]]; then
+    if [[ $VAR_DATE_REPAIR -eq 0 ]] && [[ $VAR_DATE_REPAIR_FAIL -eq 0 ]]; then
+        PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  NOTHING TO REPAIR"
+    elif [[ $VAR_DATE_REPAIR -eq 1 ]] && [[ $VAR_DATE_REPAIR_FAIL -eq 0 ]]; then
         PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  REPAIR OK"
-    elif [[ $VAR_DATE_REPAIR -eq 1 ]] && [[ $VAR_DATE_REPAIR_FAIL -eq 1 ]] && [[ $VAR_SCRIPT_VERBOSE -eq 0 ]]; then
-        PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  REPAIR OK & NOT OK"
-    elif [[ $VAR_DATE_REPAIR_FAIL -eq 1 ]] && [[ $VAR_SCRIPT_VERBOSE -eq 0 ]]; then
+    elif [[ $VAR_DATE_REPAIR -eq 1 ]] && [[ $VAR_DATE_REPAIR_FAIL -eq 1 ]]; then
+        PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  REPAIR OK & NOT OK (some succeed, some failed)"
+    elif [[ $VAR_DATE_REPAIR_FAIL -eq 1 ]]; then
         PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  REPAIR NOT OK"
     fi
     PrintMessage
