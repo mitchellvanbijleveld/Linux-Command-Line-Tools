eb377f44ff57940087528824e468fbcef3bdf0ea
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-09-16 23:20:32 +0200

    Create Daemon/Uninstall.sh

---
+++ Daemon/Uninstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+VAR_UTILITY="Daemon"
+VAR_UTILITY_SCRIPT="Install"
+VAR_UTILITY_SCRIPT_VERSION="2024.09.09-2048"
+VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo PrintMessage rm sed"
+
+# Utility Script Variables
+VAR_DAEMON_NAME="$1"
+VAR_DAEMON_CONFIG_FILE=$(echo "$VAR_UTILITY_SCRIPT_CONFIG_DIR" | sed "s/$VAR_UTILITY_SCRIPT/$VAR_DAEMON_NAME/")
+VAR_DAEMON_EXAMPLE_FILE="$VAR_UTILITY_FOLDER_PATH/Examples/$VAR_DAEMON_NAME"
+
+if [[ $VAR_DAEMON_NAME == "" ]]; then
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "No Daemon specified. Exiting..."
+    exit 1
+fi
+
+PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" $VAR_DAEMON_CONFIG_FILE
+PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" $VAR_DAEMON_EXAMPLE_FILE
+
+if [[ ! -f "$VAR_DAEMON_CONFIG_FILE" ]] && [[ $@ != *"--replace"* ]]; then
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Daemon '$VAR_DAEMON_NAME' not installed. Exiting..."
+    exit 1
+fi
+
+rm -v $VAR_DAEMON_CONFIG_FILE
+
+exit 0
