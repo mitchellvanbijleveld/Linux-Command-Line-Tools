e85ce57488742879fcc1f959f525a6084cf91a7f
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-09-16 22:54:06 +0200

    InteractiveShell/Daemon/List.sh: use case instead of if

--- InteractiveShell/Daemon/List.sh
+++ InteractiveShell/Daemon/List.sh
@@ -11,21 +11,12 @@ AvailableChoices_Add "Q" "Quit Interactive Shell"
 echo
 PrintUserChoiceQuestion
 
-if [[ $UserInput -ge 1 ]] && [[ $UserInput -le 4 ]]; then
-    case $UserInput in
+case $UserInput in
     1) StartUtilityScript "ALL";;
     2) StartUtilityScript "EXAMPLES";;
     3) StartUtilityScript "INSTALLED";;
     4) StartUtilityScript "RUNNING";;
-    esac
-elif [[ $(echo $UserInput | tr '[:lower:]' '[:upper:]')  == "B" ]]; then
-    PrintMenu_Utility $VAR_UTILITY
-elif [[ $(echo $UserInput | tr '[:lower:]' '[:upper:]')  == "Q" ]]; then
-    echo "Quiting Interactive Shell..."
-    echo
-    exit 0
-else
-    echo "I don't know how to react to your input '$UserInput'..."
-    echo
-    exit 1
-fi
+    [bB]) PrintMenu_Utility $VAR_UTILITY;;
+    [qQ]) echo "Quiting Interactive Shell..."; echo; exit 0;;
+    *) echo "I don't know how to react to your input '$UserInput'..."; echo; exit 1;;
+esac
