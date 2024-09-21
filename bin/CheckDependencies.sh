060baf7147faaed4a89d852e97507ca609f34fd0
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-09-21 20:18:41 +0200

    bin: change echoInfo and echoDebug to PrintMessage

--- bin/CheckDependencies.sh
+++ bin/CheckDependencies.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-VAR_UTILITY="bin"
+VAR_UTILITY="mitchellvanbijleveld"
 VAR_UTILITY_SCRIPT="CheckDependencies"
+VAR_UTILITY_SCRIPT_VERSION="2024.XX.XX-XXXX"
+VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="PrintMessage"
 
-echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependency check has been started..."
+PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependency check has been started..."
 
 if [[ "$@" == *"--install-check"* ]]; then
-    echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Script will write missing dependencies to file..."
+    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Script will write missing dependencies to file..."
     InstallCheck=1
 fi
 
@@ -14,12 +16,12 @@ CheckDependency(){
     # $2 bool exit
 
     if type "$1" &> /dev/null; then
-        echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependency OK     : $(printf "%-10s\n" "$1")"
+        PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependency OK     : $(printf "%-10s\n" "$1")"
     else
-        echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependency NOT OK : $(printf "%-10s\n" "$1")"
+        PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependency NOT OK : $(printf "%-10s\n" "$1")"
         VAR_MISSING_DEPENDENCIES="$VAR_MISSING_DEPENDENCIES$1 "
         if $2; then
-            echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependency '$1' was not found on this system. Exiting..."
+            PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependency '$1' was not found on this system. Exiting..."
             exit 1
         else
             DependencyMissing=1
@@ -29,27 +31,27 @@ CheckDependency(){
 }
 
 if [[ "$@" ]] && [[ ! $InstallCheck -eq 1 ]]; then
-    echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Checking specified dependencies. Exiting upon failure..."
-    echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "The following dependencies will be checked: '$@'..."
+    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Checking specified dependencies. Exiting upon failure..."
+    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "The following dependencies will be checked: '$@'..."
 
     for var_script_dependency in $@; do
         CheckDependency "$var_script_dependency" true
     done
 else
-    echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Checking all utility script dependencies..."
+    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Checking all utility script dependencies..."
 
     VAR_COMMAND_LINE_TOOLS_TO_INSTALL="$VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS"
 
     for var_utility in "$VAR_BIN_INSTALL_DIR"/*; do
-        echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Processing utility '$(basename $var_utility)'..."
+        PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Processing utility '$(basename $var_utility)'..."
         for var_utility_script in "$var_utility"/*; do
-            echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Processing utility script '$(basename ${var_utility_script%.sh})'..."
+            PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Processing utility script '$(basename ${var_utility_script%.sh})'..."
             if ! [ -f "$var_utility_script" ]; then
-                echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Not a valid file ('${var_utility_script%.sh}')! Skipping..."
+                PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Not a valid file ('${var_utility_script%.sh}')! Skipping..."
                 continue
             fi
             for var_cli in $(eval $(grep "^VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS=" $var_utility_script); echo $VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS); do
-                echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Found '$var_cli' in '$(basename $var_utility)/$(basename ${var_utility_script%.sh})'!"
+                PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Found '$var_cli' in '$(basename $var_utility)/$(basename ${var_utility_script%.sh})'!"
                 if ! [[ "$VAR_COMMAND_LINE_TOOLS_TO_INSTALL" == *"$var_cli"* ]]; then
                     VAR_COMMAND_LINE_TOOLS_TO_INSTALL="$VAR_COMMAND_LINE_TOOLS_TO_INSTALL $var_cli"
                 fi
@@ -57,7 +59,7 @@ else
         done
     done
 
-    echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Necessary command line utilities in order to work correctly: '$VAR_COMMAND_LINE_TOOLS_TO_INSTALL'"
+    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Necessary command line utilities in order to work correctly: '$VAR_COMMAND_LINE_TOOLS_TO_INSTALL'"
 
     for var_script_dependency in $VAR_COMMAND_LINE_TOOLS_TO_INSTALL; do
         CheckDependency "$var_script_dependency" false
@@ -65,19 +67,19 @@ else
 
     if ! [[ $DependencyMissing -eq 1 ]]; then
         if [[ $InstallCheck -eq 1 ]]; then
-            echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependencies OK!"
+            PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependencies OK!"
             exit 0
         fi
-        echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependencies OK!"
+        PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependencies OK!"
         exit 0
     else
         if [[ $InstallCheck -eq 1 ]]; then
-            echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Writing missing dependencies to file..."
+            PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Writing missing dependencies to file..."
             echo "$VAR_MISSING_DEPENDENCIES" > ".mvb.missing_dependencies"
-            echoDebug "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "$(ls -al .mvb.missing_dependencies)"
+            PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "$(ls -al .mvb.missing_dependencies)"
             exit 0
         fi
-        echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependencies NOT OK! Missing dependencies: $VAR_MISSING_DEPENDENCIES"
+        PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Dependencies NOT OK! Missing dependencies: $VAR_MISSING_DEPENDENCIES"
         exit 1
     fi
 fi
--- bin/update.sh
+++ bin/update.sh
@@ -4,8 +4,8 @@ VAR_UTILITY_SCRIPT="update"
 VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo git"
 
 
-echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Updating..."
-echoInfo
+PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Updating..."
+PrintMessage "INFO"
 git -C "$VAR_BIN_INSTALL_DIR" pull
-echoInfo
-echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Done"
+PrintMessage "INFO"
+PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Done"
