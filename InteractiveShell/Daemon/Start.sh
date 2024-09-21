61c0ef91c20ba916236c065f92067f1dd027546f
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-09-21 22:40:10 +0200

    InteractiveShell/Daemon: add start and stop

---
+++ InteractiveShell/Daemon/Start.sh
@@ -0,0 +1,28 @@
+echo "Please select on of the following options:"
+PrintDirectoryContent "FILES" "$VAR_BIN_CONFIG_DIR/$VAR_UTILITY"
+if [[ ${#DirectoryContent[@]} -eq 0 ]]; then
+    echo "  No daemons are installed."
+fi
+echo
+echo "Available choices:"; AvailableChoices_Reset
+if [[ ${#DirectoryContent[@]} -gt 0 ]]; then
+    AvailableChoices_Add "1-${#DirectoryContent[@]}" "Daemon to install"
+fi
+AvailableChoices_Add "B" "Back"
+AvailableChoices_Add "Q" "Quit Interactive Shell"
+echo
+PrintUserChoiceQuestion
+
+if [[ $UserInput -ge 1 ]] && [[ $UserInput -le ${#DirectoryContent[@]} ]]; then
+    StartUtilityScript $(basename ${DirectoryContent[$((UserInput - 1))]})
+elif [[ $(echo $UserInput | tr '[:lower:]' '[:upper:]')  == "B" ]]; then
+    PrintMenu_Utility $VAR_UTILITY
+elif [[ $(echo $UserInput | tr '[:lower:]' '[:upper:]')  == "Q" ]]; then
+    echo "Quiting Interactive Shell..."
+    echo
+    exit 0
+else
+    echo "Can't handle this option."
+    echo
+    exit 1
+fi
