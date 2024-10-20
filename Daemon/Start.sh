b03ce3f7beb8faf50b374f81782a72498f14d40a
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-10-20 16:52:56 +0200

    Daemon/Start.sh: replace missed echoInfo with PrintMessage

--- Daemon/Start.sh
+++ Daemon/Start.sh
@@ -10,7 +10,7 @@ VAR_DAEMON_CONFIG_FILE=$(echo "$VAR_UTILITY_SCRIPT_CONFIG_DIR" | sed "s/$VAR_UTI
 VAR_DAEMON_PID_FILE=$(echo "$VAR_UTILITY_SCRIPT_TEMP_DIR" | sed "s/$VAR_UTILITY_SCRIPT/$VAR_DAEMON_NAME/")
 
 if [[ $VAR_DAEMON_NAME == "" ]]; then
-    echoInfo "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "No Daemon specified. Exiting..."
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "No Daemon specified. Exiting..."
     exit 1
 fi
 
